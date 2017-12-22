9472d11704fa3c68b0ce85adce870656cb00bdaf
Kevin Schmidt <kevins@imagemattersllc.com>
2017-12-22 13:56:33 -0500

    Newsmap plugin initial commit - boilerplate

---
+++ plugins/newsmap/includes/class-newsmap-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.geoplatform.gov/
+ * @since      1.0.0
+ *
+ * @package    Newsmap
+ * @subpackage Newsmap/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Newsmap
+ * @subpackage Newsmap/includes
+ * @author     Kevin Schmidt <kevins@imagemattersllc.com>
+ */
+class Newsmap_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
