113bad1057914c495af374d2cac41f04aa4fe720
Lee  Heazel <leeh@imagemattersllc.com>
2018-02-15 15:51:09 -0500

    Changed database deletion trigger from deactivation to uninstallation

--- plugins/geop-maps/includes/class-geop-maps-deactivator.php
+++ plugins/geop-maps/includes/class-geop-maps-deactivator.php
@@ -26,13 +26,12 @@ class Geop_Maps_Deactivator {
 	 * Destroys the table. Here for testing, should be moved to uninstall later.
 	*/
 	private static function gpf_database_remove() {
-	     global $wpdb;
-	     $table_name = $wpdb->prefix . 'newsmap_db';
-	     $sql = "DROP TABLE IF EXISTS $table_name;";
-	     $wpdb->query($sql);
-	     delete_option("my_plugin_db_version");
+	     // global $wpdb;
+	     // $table_name = $wpdb->prefix . 'newsmap_db';
+	     // $sql = "DROP TABLE IF EXISTS $table_name;";
+	     // $wpdb->query($sql);
+	     // delete_option("my_plugin_db_version");
 	}
-
 
 
 	/**
--- plugins/geop-maps/uninstall.php
+++ plugins/geop-maps/uninstall.php
@@ -27,5 +27,10 @@
 
 // If uninstall not called from WordPress, then exit.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	global $wpdb;
+	$table_name = $wpdb->prefix . 'newsmap_db';
+	$sql = "DROP TABLE IF EXISTS $table_name;";
+	$wpdb->query($sql);
+	delete_option("my_plugin_db_version");
 	exit;
 }
