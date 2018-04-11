f210b45721c9bc81a81d6d7f866cf87b6715f65f
Kevin Schmidt <kevins@imagemattersllc.com>
2018-04-11 00:47:20 -0400

    got rid of section causing PHP warning

--- themes/GeoPlatform2018-3.0.1/index.php
+++ themes/GeoPlatform2018-3.0.1/index.php
@@ -7,9 +7,6 @@ https://www.taniarascia.com/developing-a-wordpress-theme-from-scratch/ -->
   <!--GeoPlatform header with intro -->
     <?php get_template_part( 'gp_intro', get_post_format() ); ?>
 
-<!--Featured Services section-->
-<!--<?php// get_template_part( 'featured', get_post_format() ); ?>-->
-<!--WP backend test section-->
   <?php get_template_part( 'main-page', get_post_format() ); ?>
 
   <?php $customizerLink = get_theme_mod( 'Map_Gallery_link_box' );?>
