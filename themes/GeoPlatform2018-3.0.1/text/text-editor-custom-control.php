3a7f15bee64b64281628a2e6205a8d6a41a0ffd3
Kevin Schmidt <kevins@imagemattersllc.com>
2018-04-18 11:31:11 -0400

    pre-populate text editor banner area with our text

--- themes/GeoPlatform2018-3.0.1/text/text-editor-custom-control.php
+++ themes/GeoPlatform2018-3.0.1/text/text-editor-custom-control.php
@@ -11,18 +11,24 @@ class Text_Editor_Custom_Control extends WP_Customize_Control
        */
       public function render_content()
        {
+            $content = $this->value();
             ?>
                 <label>
                   <span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
-                  <input type="hidden" <?php $this->link(); ?> value="<?php echo esc_textarea( $this->value() ); ?>">
+                  <input type="hidden" <?php $this->link(); ?> value="<?php echo esc_textarea( $content ); ?>">
                   <?php
+                    if (!$content){
+                      $content = "<h1 style='text-align: center'>Your Community's Title</h1>
+<p style='text-align: center'>Create and manage your own Dynamic Digital Community on the GeoPlatform!</p>
+<p style='text-align: right'></p>";
+                    }
                     $settings = array(
                         'textarea_name' => $this->id,
                         'media_buttons' => false,
                         'drag_drop_upload' => false,
                         'teeny' => false
                     );
-                    wp_editor($this->value(), $this->id, $settings );
+                    wp_editor($content, $this->id, $settings );
 
                     do_action('admin_footer');
                     do_action('admin_print_footer_scripts');
