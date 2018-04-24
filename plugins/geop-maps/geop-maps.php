c54c80d09d97ec9fa8354b0896d0868f8f741254
Lee  Heazel <leeh@imagemattersllc.com>
2018-04-24 14:26:56 -0400

    Changed the hyperlink in the Geop-maps plugin admin instructions to open in a new tab instead of current. Update to v1.0.4. Wordpress distribution imminent.

--- plugins/geop-maps/geop-maps.php
+++ plugins/geop-maps/geop-maps.php
@@ -16,7 +16,7 @@
  * Plugin Name:       GeoPlatform Maps Plugin
  * Plugin URI:        www.geoplatform.gov
  * Description:       Manage your own personal database of GeoPlatform interactive maps and use shortcode to insert them into your posts.
- * Version:           1.0.2
+ * Version:           1.0.3
  * Author:            Image Matters LLC
  * Author URI:        www.geoplatform.gov
  * License:           GPL-2.0+
