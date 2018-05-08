1848ee91a6c7c06545f76ce4cb147ef0b4326fb1
Kevin Schmidt <kevins@imagemattersllc.com>
2018-05-08 00:33:44 -0400

    updates from theme unit testing

--- themes/geoplatform-ccb/page-templates/page-blog.php
+++ themes/geoplatform-ccb/page-templates/page-blog.php
@@ -16,11 +16,12 @@
     		$temp = $wp_query; $wp_query= null;
     		$wp_query = new WP_Query(); $wp_query->query('posts_per_page=5' . '&paged='.$paged);
     		while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
     		<h3><a href="<?php the_permalink(); ?>" title="Read more"><?php the_title(); ?></a></h3>
         <h4><?php echo get_the_date(); ?></h4>
     		<?php the_excerpt(); ?>
-
+        </div>
     		<?php endwhile; ?>
 
     		<?php if ($paged > 1) { ?>
