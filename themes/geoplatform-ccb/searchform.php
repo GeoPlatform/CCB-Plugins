6a3221f74d41951b7812a80efb03e98bcc76b7d7
Kevin Schmidt <kevins@imagemattersllc.com>
2018-06-03 04:24:59 -0400

    fixed non-character showing up per theme check review

--- themes/geoplatform-ccb/searchform.php
+++ themes/geoplatform-ccb/searchform.php
@@ -8,7 +8,7 @@ http://buildwpyourself.com/wordpress-search-form-template/ -->
 <!-- <form role="search" method="get" id="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <div class="search-wrap">
     	<label class="screen-reader-text" for="s"><?php _e( 'Search for:', 'geoplatform-ccb' ); ?></label>
-        <input type="search" placeholder="<?php echo esc_attr( 'Search…', 'geoplatform-ccb' ); ?>"
+        <input type="search" placeholder="<?php echo esc_attr( 'Search...', 'geoplatform-ccb' ); ?>"
         name="s" id="search-input" value="<?php echo esc_attr( get_search_query() ); ?>" />
         <input class="screen-reader-text" type="submit" id="search-submit" value="Search" />
     </div>
@@ -17,7 +17,7 @@ http://buildwpyourself.com/wordpress-search-form-template/ -->
 <form role="search" method="get" id="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 <div class="input-group-slick">
       <span class="glyphicon glyphicon-search"></span>
-      <input type="search" class="form-control" placeholder="<?php echo esc_attr( 'Search…', 'geoplatform-ccb' ); ?>"
+      <input type="search" class="form-control" placeholder="<?php echo esc_attr( 'Search...', 'geoplatform-ccb' ); ?>"
       name="s" id="search-input" value="<?php echo esc_attr( get_search_query() ); ?>"/>
       <button type="submit" class="btn btn-default" id="search-submit">Search</button>
   </div>
