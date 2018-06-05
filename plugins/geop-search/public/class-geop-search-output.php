0cd79fb3dcbed7b21777da25d4373e080c7f8d4a
Patrick Neal <patrickn@imagemattersllc.com>
2018-06-05 14:29:53 -0400

    updated output php to fix ordering of elements; updated distro files for plugin

--- plugins/geop-search/public/class-geop-search-output.php
+++ plugins/geop-search/public/class-geop-search-output.php
@@ -1,10 +1,11 @@
 
+<app-root></app-root>
 
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
-  <link href="public/css/styles.bundle.css" rel="stylesheet" />
-  <link href="public/css/geop-search-styles.css" rel="stylesheet" />
-  <script type="text/javascript" src="<?php echo plugin_dir_url( __FILE__ ) . 'js/inline.bundle.js' ?>"></script>
-  <script type="text/javascript" src="<?php echo plugin_dir_url( __FILE__ ) . 'js/polyfills.bundle.js' ?>"></script>
-  <script type="text/javascript" src="<?php echo plugin_dir_url( __FILE__ ) . 'js/scripts.bundle.js' ?>"></script>
-  <script type="text/javascript" src="<?php echo plugin_dir_url( __FILE__ ) . 'js/main.bundle.js' ?>"></script>
-  <app-root></app-root>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+<link href="public/css/styles.bundle.css" rel="stylesheet" />
+<link href="public/css/geop-search-styles.css" rel="stylesheet" />
+
+<script type="text/javascript" src="<?php echo plugin_dir_url( __FILE__ ) . 'js/inline.bundle.js' ?>"></script>
+<script type="text/javascript" src="<?php echo plugin_dir_url( __FILE__ ) . 'js/polyfills.bundle.js' ?>"></script>
+<script type="text/javascript" src="<?php echo plugin_dir_url( __FILE__ ) . 'js/scripts.bundle.js' ?>"></script>
+<script type="text/javascript" src="<?php echo plugin_dir_url( __FILE__ ) . 'js/main.bundle.js' ?>"></script>
