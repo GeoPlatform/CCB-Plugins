1cc6b5c1bafbdca604701b4253b7a289c2c9914e
Lee  Heazel <leeh@imagemattersllc.com>
2018-06-12 12:35:03 -0400

    DT-1826: Category images functional in GeoPlatform-CCB. Non-functional URL rewrite code removed from geoplatform-search.

--- plugins/geoplatform-search/geoplatform-search.php
+++ plugins/geoplatform-search/geoplatform-search.php
@@ -55,25 +55,6 @@ function deactivate_geop_search() {
 	Geop_Search_Deactivator::deactivate();
 }
 
-
-
-
-// add_action( 'init', 'wpse9870_init_external' );
-// function wpse9870_init_external()
-// {
-//     global $wp_rewrite;
-//     $plugin_url = plugins_url( 'geoplatform-search-page-template.php', __FILE__ );
-//     $plugin_url = substr( $plugin_url, strlen( home_url() ) + 1 );
-//     // The pattern is prefixed with '^'
-//     // The substitution is prefixed with the "home root", at least a '/'
-//     // This is equivalent to appending it to `non_wp_rules`
-//     $wp_rewrite->add_external_rule( 'sample-page$', $plugin_url );
-// }
-
-
-
-
-
 // Applies our custom page template to the created page.
 add_filter('page_template', 'geopsearch_page_template');
 function geopsearch_page_template($page_template) {
