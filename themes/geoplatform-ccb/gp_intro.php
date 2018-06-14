6016f076f57d837a7a2ab60241be00794c98de36
Kevin Schmidt <kevins@imagemattersllc.com>
2018-06-14 08:12:47 -0400

    cleaned up main banner

--- themes/geoplatform-ccb/gp_intro.php
+++ themes/geoplatform-ccb/gp_intro.php
@@ -5,12 +5,11 @@
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-12 col-xs-12">
                   <!-- WYSIWYG Text area. See functions.php -> text_editor_setting() for details
                   Current area modeled off of Geoplatform styling -->
-                          <?php echo wp_kses_post(get_theme_mod( 'text_editor_setting'));
-                          //var_dump(get_theme_mod('text_editor_setting'));?>
+                          <?php echo wp_kses_post(get_theme_mod( 'text_editor_setting' )); ?>
                   <div class="row">
                     <br />
-                    <?php $c2a_button = get_theme_mod('call2action_button', true);
-                          $c2a_url = get_theme_mod('call2action_url', "https://geoplatform.gov/about");
+                    <?php $c2a_button = get_theme_mod('call2action_button', 'true');
+                          $c2a_url = get_theme_mod('call2action_url','http://geoplatform.gov');
                     ?>
                     <?php if ($c2a_button === true) { ?>
                       <div class="text-centered">
