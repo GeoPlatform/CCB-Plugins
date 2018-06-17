18bee33f61af765867c24647fe6b6bdacafa3bab
Kevin Schmidt <kevins@imagemattersllc.com>
2018-06-16 21:50:05 -0400

    updated styling

--- themes/geoplatform-ccb/functions.php
+++ themes/geoplatform-ccb/functions.php
@@ -29,6 +29,7 @@ function geop_ccb_scripts() {
   wp_enqueue_style( 'custom-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'theme-style', get_template_directory_uri() . '/css/Geomain_style.css' );
 	wp_enqueue_style( 'bootstrap-css',get_template_directory_uri() . '/css/bootstrap.css');
+	wp_enqueue_style( 'theme-style', get_template_directory_uri() . '/css/Geomain_style.css' );
 	wp_enqueue_script( 'bootstrap-min', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '3.3.7', true);
   //wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.js', array(), '3.3.7', true);
 	wp_enqueue_script( 'geoplatform-ccb-js', get_template_directory_uri() . '/js/geoplatform.style.js', array('jquery'), null, true );
