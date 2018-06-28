c7d0194d234c3fe28aa2ad0bca61afb156ace934
Lee  Heazel <leeh@imagemattersllc.com>
2018-06-28 13:47:28 -0400

    Heavy modification coming to search button. Pre-work commit with pretty indentations.

--- themes/geoplatform-portal/gpsearch.php
+++ themes/geoplatform-portal/gpsearch.php
@@ -1,36 +1,37 @@
 <div class="search section--linked">
 
-<h4 class="heading">
-<div class="line"></div>
-<div class="line-arrow"></div>
-</h4>
-<br>
+  <h4 class="heading">
+    <div class="line"></div>
+    <div class="line-arrow"></div>
+  </h4>
 
-<div class="container-fluid">
-<div class="row">
-<div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
-<div class="input-group-slick input-group-slick--lg">
-<span class="glyphicon glyphicon-search"></span>
-<input id="geoplatformsearchfield" type="text" placeholder="Search the GeoPlatform" class="form-control input-lg"> 
-<button type="button" class="btn btn-primary" 
-onclick="window.location.href='geoplatform-search/#/?q='+jQuery('#geoplatformsearchfield').val()">
-Search
-</button>
-<!--
-^ THIS BUTTON SHOULD SEND USER TO THE SEARCH PLUGIN PAGE WHEN CLICKED AND SHOULD SEND 
-THE CONTENTS OF THE INPUT ELEMENT ABOVE IT AS THE "q=" QUERY STRING PARAMETER.
--->
-</div>
-</div>
-</div>
-</div>
+  <br>
 
-<br>
-<br>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+        <div class="input-group-slick input-group-slick--lg">
+        <span class="glyphicon glyphicon-search"></span>
+        <input id="geoplatformsearchfield" type="text" placeholder="Search the GeoPlatform" class="form-control input-lg"> 
+        <button type="button" class="btn btn-primary"
+          onclick="window.location.href='geoplatform-search/#/?q='+jQuery('#geoplatformsearchfield').val()">
+          Search
+        </button>
+        <!--
+        ^ THIS BUTTON SHOULD SEND USER TO THE SEARCH PLUGIN PAGE WHEN CLICKED AND SHOULD SEND
+        THE CONTENTS OF THE INPUT ELEMENT ABOVE IT AS THE "q=" QUERY STRING PARAMETER.
+        -->
+        </div>
+      </div>
+    </div>
+  </div>
 
-<div class="footing">
-<div class="line-cap"></div>
-<div class="line"></div>
-</div>
+  <br>
+  <br>
+
+  <div class="footing">
+    <div class="line-cap"></div>
+    <div class="line"></div>
+  </div>
 
 </div>
