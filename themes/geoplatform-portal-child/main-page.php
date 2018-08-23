855aecc845c6f0cf9948b57ac8efc83f7160a819
Lee  Heazel <leeh@imagemattersllc.com>
2018-08-23 16:02:40 -0400

    Most of the way there for Portal.

--- themes/geoplatform-portal-child/main-page.php
+++ themes/geoplatform-portal-child/main-page.php
@@ -19,6 +19,26 @@ class Geopportal_MainPage_Widget extends WP_Widget {
       $geopportal_mainpage_disp_title = apply_filters('widget_title', $instance['geopportal_mainpage_title']);
 		else
       $geopportal_mainpage_disp_title = "Features &amp; Announcements";
+
+		if (array_key_exists('geopportal_mainpage_first_link', $instance) && isset($instance['geopportal_mainpage_first_link']) && !empty($instance['geopportal_mainpage_first_link']))
+      $geopportal_mainpage_disp_first_link = apply_filters('widget_title', $instance['geopportal_mainpage_first_link']);
+		else
+      $geopportal_mainpage_disp_first_link = "";
+		if (array_key_exists('geopportal_mainpage_first_content', $instance) && isset($instance['geopportal_mainpage_first_content']) && !empty($instance['geopportal_mainpage_first_content']))
+			$geopportal_mainpage_disp_first_content = apply_filters('widget_title', $instance['geopportal_mainpage_first_content']);
+		else
+			$geopportal_mainpage_disp_first_content = "";
+
+		if (array_key_exists('geopportal_mainpage_second_link', $instance) && isset($instance['geopportal_mainpage_second_link']) && !empty($instance['geopportal_mainpage_second_link']))
+      $geopportal_mainpage_disp_second_link = apply_filters('widget_title', $instance['geopportal_mainpage_second_link']);
+		else
+      $geopportal_mainpage_disp_second_link = "";
+		if (array_key_exists('geopportal_mainpage_third_link', $instance) && isset($instance['geopportal_mainpage_third_link']) && !empty($instance['geopportal_mainpage_third_link']))
+      $geopportal_mainpage_disp_third_link = apply_filters('widget_title', $instance['geopportal_mainpage_third_link']);
+		else
+      $geopportal_mainpage_disp_third_link = "";
+
+
     echo $args['before_widget'];?>
 
     <div class="whatsNew section--linked">
@@ -33,24 +53,14 @@ class Geopportal_MainPage_Widget extends WP_Widget {
             </div>
           </h4>
           <div class="row">
-            <?php
-             //set counter
-              $postNum = 0;
-              //only show the first two posts
-              while ( have_posts() && ($postNum < 3)) : the_post();
-                  $postNum++;
-                  get_template_part( 'post-card', get_post_format() );
-              endwhile;
-              ?>
+						<?php
+						$geopportal_mainpage_disp_post_array = array($geopportal_mainpage_disp_first_link, $geopportal_mainpage_disp_second_link, $geopportal_mainpage_disp_third_link);
+						$geopportal_mainpage_disp_content_array = array($geopportal_mainpage_disp_first_content, $geopportal_mainpage_disp_first_content, $geopportal_mainpage_disp_first_content);
+						for ($x = 0; $x <= 2; $x++){
+							include( locate_template('post-card.php', false, false));
+						}
+            ?>
             <div class="clearfix"></div>
-          </div><!--#row-->
-          <div class="row">
-            <nav>
-              <ul class="pager">
-                <li><?php next_posts_link( 'Previous' ); ?></li>
-                <li><?php previous_posts_link( 'Next' ); ?></li>
-              </ul>
-            </nav>
           </div><!--#row-->
         </div><!--#col-lg-10 col-lg-offset-1-->
       </div><!--#container-fluid-->
@@ -67,24 +77,90 @@ class Geopportal_MainPage_Widget extends WP_Widget {
   // The admin side of the widget.
 	public function form( $instance ) {
 
+		// Checks if the Content Boxes plugin is installed.
+		$geopportal_mainpage_cb_bool = false;
+		$geopportal_mainpage_cb_message = "Content Blocks plugin not found.";
+		if (in_array( 'custom-post-widget/custom-post-widget.php', (array) get_option( 'active_plugins', array() ) )){
+			$geopportal_mainpage_cb_bool = true;
+			$geopportal_mainpage_cb_message = "Click here to edit this content block";
+		}
+
     // Checks for entries in the widget admin boxes and provides defaults if empty.
     $geopportal_mainpage_title = ! empty( $instance['geopportal_mainpage_title'] ) ? $instance['geopportal_mainpage_title'] : 'Features &amp; Announcements';
-    ?>
+		$geopportal_mainpage_first_link = ! empty( $instance['geopportal_mainpage_first_link'] ) ? $instance['geopportal_mainpage_first_link'] : '';
+		$geopportal_mainpage_first_content = ! empty( $instance['geopportal_mainpage_first_content'] ) ? $instance['geopportal_mainpage_first_content'] : '';
+		$geopportal_mainpage_second_link = ! empty( $instance['geopportal_mainpage_second_link'] ) ? $instance['geopportal_mainpage_second_link'] : '';
+		$geopportal_mainpage_third_link = ! empty( $instance['geopportal_mainpage_third_link'] ) ? $instance['geopportal_mainpage_third_link'] : '';
+
+		// Sets up the top-left content box link, or just a home link if invalid.
+		if (array_key_exists('geopportal_mainpage_first_content', $instance) && isset($instance['geopportal_mainpage_first_content']) && !empty($instance['geopportal_mainpage_first_content']) && $geopportal_mainpage_cb_bool){
+    	$geopportal_mainpage_first_temp_url = preg_replace('/\D/', '', $instance[ 'geopportal_mainpage_first_content' ]);
+    	if (is_numeric($geopportal_mainpage_first_temp_url))
+      	$geopportal_mainpage_first_url = home_url() . "/wp-admin/post.php?post=" . $geopportal_mainpage_first_temp_url . "&action=edit";
+    	else
+      	$geopportal_mainpage_first_url = home_url();
+		}
+		else
+			$geopportal_mainpage_first_url = home_url();
+
+
+		?>
 
 <!-- HTML for the widget control box. -->
     <p>
       <label for="<?php echo $this->get_field_id( 'geopportal_mainpage_title' ); ?>">Title:</label>
       <input type="text" id="<?php echo $this->get_field_id( 'geopportal_mainpage_title' ); ?>" name="<?php echo $this->get_field_name( 'geopportal_mainpage_title' ); ?>" value="<?php echo esc_attr( $geopportal_mainpage_title ); ?>" />
     </p>
+		<hr>
 		<p>
-		  <?php _e('There are numerous changes to this widget planned for a future release, including resource sorting, prioritizing, exclusion and inclusion. For now, an editable title is the only feature.', 'geoplatform-ccb'); ?>
+			<?php _e('The boxes below accept the slugs of the linked post or page. Please ensure that any input slugs are valid.', 'geoplatform-ccb'); ?>
+		</p>
+		<p>
+      <label for="<?php echo $this->get_field_id( 'geopportal_mainpage_first_link' ); ?>">First Post Slug:</label>
+      <input type="text" id="<?php echo $this->get_field_id( 'geopportal_mainpage_first_link' ); ?>" name="<?php echo $this->get_field_name( 'geopportal_mainpage_first_link' ); ?>" value="<?php echo esc_attr( $geopportal_mainpage_first_link ); ?>" />
+    </p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'geopportal_mainpage_first_content' ); ?>">First Content Block Shortcode:</label><br>
+			<input type="text"  id="<?php echo $this->get_field_id( 'geopportal_mainpage_first_content' ); ?>" name="<?php echo $this->get_field_name( 'geopportal_mainpage_first_content' ); ?>" value="<?php echo esc_attr($geopportal_mainpage_first_content); ?>" />
+			<a href="<?php echo esc_url($geopportal_mainpage_first_url); ?>" target="_blank"><?php _e($geopportal_mainpage_cb_message, 'geoplatform-ccb') ?></a><br>
+		</p>
+		<hr>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'geopportal_mainpage_second_link' ); ?>">Second Post Slug:</label>
+			<input type="text" id="<?php echo $this->get_field_id( 'geopportal_mainpage_second_link' ); ?>" name="<?php echo $this->get_field_name( 'geopportal_mainpage_second_link' ); ?>" value="<?php echo esc_attr( $geopportal_mainpage_second_link ); ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'geopportal_mainpage_third_link' ); ?>">Third Post Slug:</label>
+			<input type="text" id="<?php echo $this->get_field_id( 'geopportal_mainpage_third_link' ); ?>" name="<?php echo $this->get_field_name( 'geopportal_mainpage_third_link' ); ?>" value="<?php echo esc_attr( $geopportal_mainpage_third_link ); ?>" />
 		</p>
 		<?php
 	}
 
 	public function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
+
+		// Checks if the Content Boxes plugin is installed.
+		$geopportal_mainpage_cb_bool = false;
+		if (in_array( 'custom-post-widget/custom-post-widget.php', (array) get_option( 'active_plugins', array() ) ))
+			$geopportal_mainpage_cb_bool = true;
+
     $instance[ 'geopportal_mainpage_title' ] = strip_tags( $new_instance[ 'geopportal_mainpage_title' ] );
+		$instance[ 'geopportal_mainpage_first_link' ] = strip_tags( $new_instance[ 'geopportal_mainpage_first_link' ] );
+		$instance[ 'geopportal_mainpage_first_content' ] = strip_tags( $new_instance[ 'geopportal_mainpage_first_content' ] );
+		$instance[ 'geopportal_mainpage_second_link' ] = strip_tags( $new_instance[ 'geopportal_mainpage_second_link' ] );
+		$instance[ 'geopportal_mainpage_third_link' ] = strip_tags( $new_instance[ 'geopportal_mainpage_third_link' ] );
+
+		// Validity check for the content box URL.
+		if (array_key_exists('geopportal_mainpage_first_content', $instance) && isset($instance['geopportal_mainpage_first_content']) && !empty($instance['geopportal_mainpage_first_content']) && $geopportal_mainpage_cb_bool){
+    	$geopportal_mainpage_first_temp_url = preg_replace('/\D/', '', $instance[ 'geopportal_mainpage_first_content' ]);
+    	if (is_numeric($geopportal_mainpage_first_temp_url))
+      	$geopportal_mainpage_first_url = home_url() . "/wp-admin/post.php?post=" . $geopportal_mainpage_first_temp_url . "&action=edit";
+    	else
+      	$geopportal_mainpage_first_url = home_url();
+		}
+		else
+			$geopportal_mainpage_first_url = home_url();
+
 
 		return $instance;
 	}
--- themes/geoplatform-portal-child/post-card.php
+++ themes/geoplatform-portal-child/post-card.php
@@ -6,42 +6,52 @@
  *
  * @since 3.0.0
  */
+// $geopportal_mainpage_feat_page = get_page_by_path($geopportal_mainpage_disp_array[$x]);
+// echo $geopportal_mainpage_disp_array[$x];
+// echo "  ";
+// echo get_the_title($geopportal_mainpage_feat_page);
+// echo "<br>"
+
+$geopportal_mainpage_feat_page = get_page_by_path($geopportal_mainpage_disp_post_array[$x], OBJECT, 'post');
+echo $geopportal_mainpage_disp_post_array[$x] . " ";
+echo apply_filters('the_excerpt', get_post_field('post_excerpt', $geopportal_mainpage_feat_page->ID)) . " ";
+echo "<br>"
 ?>
 
 <div class="col-sm-6 col-md-4">
 <div class="gp-ui-card gp-ui-card--md gp-ui-card">
-  <?php if ( has_post_thumbnail() ) {?>
-    <a class="media embed-responsive embed-responsive-16by9" href="<?php the_permalink(); ?>"
+  <?php if ( has_post_thumbnail($geopportal_mainpage_feat_page) ) {?>
+    <a class="media embed-responsive embed-responsive-16by9" href="<?php echo get_the_permalink($geopportal_mainpage_feat_page); ?>"
         title="<?php _e( 'Register for the Geospatial Platform Workshop', 'geoplatform-ccb') ?> ">
 
-        <img class="embed-responsive-item" src="<?php the_post_thumbnail_url(); ?>" >
+        <img class="embed-responsive-item" src="<?php echo get_the_post_thumbnail_url($geopportal_mainpage_feat_page); ?>" >
 
     </a>
     <div class="gp-ui-card__body">
-        <div class="text--primary"><?php the_title(); ?></div>
+        <div class="text--primary"><?php echo get_the_title($geopportal_mainpage_feat_page); ?></div>
         <div class="text--secondary"></div>
         <div class="text--supporting">
-            <?php the_excerpt(); ?>
+            <?php echo do_shortcode($geopportal_mainpage_disp_content_array[$x]); ?>
         </div>
     </div>
 
     <div class="gp-ui-card__footer">
         <div class="pull-right">
-            <a href="<?php the_permalink(); ?>" class="btn btn-link pull-right"><?php _e( 'Learn More', 'geoplatform-ccb'); ?></a>
+            <a href="<?php echo get_the_permalink($geopportal_mainpage_feat_page); ?>" class="btn btn-link pull-right"><?php _e( 'Learn More', 'geoplatform-ccb'); ?></a>
         </div>
     </div>
 
     <?php } else {?>
       <div class="gp-ui-card__body">
-          <div class="text--primary"><?php the_title(); ?></div>
+          <div class="text--primary"><?php echo get_the_title($geopportal_mainpage_feat_page); ?></div>
           <div class="text--secondary"></div>
           <div class="text--supporting">
-              <?php the_excerpt(); ?>
+              <?php echo do_shortcode($geopportal_mainpage_disp_content_array[$x]); ?>
           </div>
       </div>
       <div class="gp-ui-card__footer">
           <div class="pull-right">
-              <a href="<?php the_permalink(); ?>" class="btn btn-link pull-right"><?php _e( 'Learn More', 'geoplatform-ccb'); ?></a>
+              <a href="<?php echo get_the_permalink($geopportal_mainpage_feat_page); ?>" class="btn btn-link pull-right"><?php _e( 'Learn More', 'geoplatform-ccb'); ?></a>
           </div>
       </div>
   <?php } ?>
