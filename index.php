ea726d74ee5058636504915b87c751f5a5f8672b
Lee  Heazel <leeh@imagemattersllc.com>
2018-11-15 12:49:54 -0500

    progress?

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+  <header>
+    <title>This is title</title>
+  </header>
+  <body>
+    <p>Hello World in HTML!!</p>
+    <p><?php echo "Hello World in PHP!!" ?></p>
+    <p><?php phpinfo();?></p>
+  </body>
+</html>
