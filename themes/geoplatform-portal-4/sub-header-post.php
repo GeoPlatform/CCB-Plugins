320903650bea2c98cfae1c1f1186606bf3f4bbf5
Lee  Heazel <leeh@imagemattersllc.com>
2018-12-10 15:13:31 -0500

    Checking improved for sub-header dropdown operations.

--- themes/geoplatform-portal-4/sub-header-post.php
+++ themes/geoplatform-portal-4/sub-header-post.php
@@ -13,16 +13,19 @@ while ($geopportal_breadcrumb_post->post_parent){
 // Excerpt grabber and size-checker for header. Not complex, but makes me feel clever.
 // If the exceprt is larger than 300 characters, breaks it in half while making
 // sure that the first half is as close to 300 as possible without going over.
-$geop_portal_excerpt_one = wp_kses_post(get_post_meta($post->ID, 'geop_ccb_custom_wysiwyg', true));
-$geop_portal_excerpt_two = "";
+$geop_portal_excerpt_overflow = false;
+$geop_portal_excerpt_one = wp_strip_all_tags(wp_kses_post(get_post_meta($post->ID, 'geop_ccb_custom_wysiwyg', true)));
+$geop_portal_excerpt_two = wp_kses_post(get_post_meta($post->ID, 'geop_ccb_custom_wysiwyg', true));
 
 if (strlen($geop_portal_excerpt_one) > 300){
-  $geop_portal_excerpt_two = $geop_portal_excerpt_one;
+  $geop_portal_excerpt_overflow = true;
   $geop_portal_exploded_array = explode('.', $geop_portal_excerpt_one);
   $geop_portal_excerpt_one = array_shift($geop_portal_exploded_array) . '.';
   while (count($geop_portal_exploded_array) > 0 && (strlen($geop_portal_excerpt_one) + strlen($geop_portal_exploded_array[0]) < 300))
     $geop_portal_excerpt_one = $geop_portal_excerpt_one . array_shift($geop_portal_exploded_array) . '.';
-  // $geop_portal_excerpt_two = implode('.', $geop_portal_exploded_array);
+}
+else {
+  $geop_portal_excerpt_one = $geop_portal_excerpt_two;
 }
 ?>
 
@@ -40,15 +43,17 @@ if (strlen($geop_portal_excerpt_one) > 300){
 <?php if (wp_kses_post(get_post_meta($post->ID, 'geop_ccb_custom_wysiwyg', true)) != '' ){
 ?>
 <div class="m-page-overview">
-  <?php echo $geop_portal_excerpt_one; ?>
-  <div class="m-page-overview__toggle" onclick="toggleClass('.m-page-overview__additional','is-expanded m-page-overview__additive'), toggleClass('.m-page-overview','is-collapsed')">
-    <span class="fas fa-caret-down"></span>
+  <?php echo $geop_portal_excerpt_one;
+  if ($geop_portal_excerpt_overflow){ ?>
+    <div class="m-page-overview__toggle" onclick="toggleClass('.m-page-overview__additional','is-expanded m-page-overview__additive'), toggleClass('.m-page-overview','is-collapsed')">
+      <span class="fas fa-caret-down"></span>
+    </div>
   </div>
-</div>
-<div class="m-page-overview__additional">
-  <?php echo $geop_portal_excerpt_two ?>
-  <div class="m-page-overview__toggle" onclick="toggleClass('.m-page-overview__additional','is-expanded m-page-overview__additive'), toggleClass('.m-page-overview','is-collapsed')">
-    <span class="fas fa-caret-down"></span>
-  </div>
+  <div class="m-page-overview__additional">
+    <?php echo $geop_portal_excerpt_two ?>
+    <div class="m-page-overview__toggle" onclick="toggleClass('.m-page-overview__additional','is-expanded m-page-overview__additive'), toggleClass('.m-page-overview','is-collapsed')">
+      <span class="fas fa-caret-down"></span>
+    </div>
+  <?php } ?>
 </div>
 <?php } ?>
