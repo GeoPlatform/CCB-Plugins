3679014784d902ff3e52235feab0855faac31d11
Lee  Heazel <leeh@imagemattersllc.com>
2019-02-13 10:10:22 -0500

    DT-2307: Prototype global settings added to Items template in Portal Four only.

--- themes/geoplatform-portal-four/page-geoplatform-items.php
+++ themes/geoplatform-portal-four/page-geoplatform-items.php
@@ -13,13 +13,22 @@ get_header();
 
 global $wp_query;
 global $wp;
+
+$geopitems_idp = isset($_ENV['idp_url']) ? $_ENV['idp_url'] : 'https://idp.geoplatform.gov';
 ?>
 
-<script> 
-    window.GeoPlatformPluginEnv = { 
-        wpUrl: "<?php bloginfo('wpurl') ?>" 
-    }; 
+<script>
+  window.GeoPlatformPluginEnv = {
+      wpUrl: "<?php bloginfo('wpurl') ?>"
+  };
+  window.GeoPlatform = {
+    IDP_BASE_URL: "<?php echo $geopitems_idp ?>", // Where IDP is
+    APP_BASE_URL: "<?php echo home_url() ?>", // root dir for site (ex: 'https://geoplatform.gov' or 'https://communities.geoplatform.gov/ngda-wildbeasts'
+    LOGIN_URL: "<?php echo wp_login_url() ?>",
+    LOGOUT_URL: "<?php echo wp_logout_url() ?>",
+  };
 </script>
+
 <app-root></app-root>
 
 <?php get_footer(); ?>
