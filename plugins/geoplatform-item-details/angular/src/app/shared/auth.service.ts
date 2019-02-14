08d7e2805ee6fd37f8e211295d5a4d847655369e
Patrick Neal <patrickn@imagemattersllc.com>
2019-02-14 14:15:11 -0500

    log auth settings for debugging integration

--- plugins/geoplatform-item-details/angular/src/app/shared/auth.service.ts
+++ plugins/geoplatform-item-details/angular/src/app/shared/auth.service.ts
@@ -44,13 +44,14 @@ const authServiceFactory = function() {
     //https://geoplatform.atlassian.net/browse/DT-2307
     if( (<any>window).GeoPlatform ) {
         var gp = (<any>window).GeoPlatform;
-        console.log("Configuring OAuth using GeoPlatform global: ");
-        console.log(gp);
         if(gp.IDP_BASE_URL) authSettings.IDP_BASE_URL = gp.IDP_BASE_URL;
         if(gp.APP_BASE_URL) authSettings.APP_BASE_URL = gp.APP_BASE_URL;
         if(gp.LOGIN_URL) authSettings.LOGIN_URL = gp.LOGIN_URL;
         if(gp.LOGOUT_URL) authSettings.LOGOUT_URL = gp.LOGOUT_URL;
     }
+
+    console.log("Configuring OAuth using: ");
+    console.log(authSettings);
 
     return ngGpoauthFactory(authSettings);
 };
