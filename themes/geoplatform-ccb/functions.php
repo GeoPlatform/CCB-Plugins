cadf13b446019091d9aed6386b682f939184eca7
Lee  Heazel <leeh@imagemattersllc.com>
2019-05-02 14:56:26 -0400

    DT-2534: Featured image support for banners in place.

--- themes/geoplatform-ccb/functions.php
+++ themes/geoplatform-ccb/functions.php
@@ -79,9 +79,14 @@ if ( ! function_exists ( 'geopccb_enqueue_bootstrap' ) ) {
 if ( ! function_exists ( 'geop_ccb_header_image_method' ) ) {
 	function geop_ccb_header_image_method() {
 		// wp_enqueue_style('custom-style', get_template_directory_uri() . '/css/Geomain_style.css');
-			$geopccb_headerImage = get_header_image();
-      if (! $geopccb_headerImage)
-        $geopccb_headerImage = get_template_directory_uri() . "/img/default-banner.png";
+      $geopccb_headerImage = get_template_directory_uri() . "/img/default-banner.png";
+
+      if (is_singular() && get_the_post_thumbnail_url()){
+        $geopccb_headerImage = get_the_post_thumbnail_url();
+      }
+      elseif (get_header_image()){
+			  $geopccb_headerImage = get_header_image();
+      }
 
 			$geopccb_custom_css = "
 					.widget-banner-main{
