6c6eee72e2cda1a3733837d617ac42c95b95a2f4
Lee  Heazel <leeh@imagemattersllc.com>
2019-06-04 10:59:16 -0400

    Fix to bug with inactive drop-downs in Angular pages.

--- plugins/geoplatform-service-collector/public/class-geoplatform-service-collector-public.php
+++ plugins/geoplatform-service-collector/public/class-geoplatform-service-collector-public.php
@@ -97,8 +97,12 @@ class Geoplatform_Service_Collector_Public {
 		 */
 
 		wp_enqueue_script( 'jquery' );
+
+		if (wp_get_theme()->Name != "Geoplatform CCB" && wp_get_theme()->Name != "Geoplatform Portal 4.0")
+			wp_enqueue_script( 'geop_bootstrap-js', plugin_dir_url( __FILE__ ) . 'js/bootstrap.bundle.min.js', array( 'jquery' ), $this->version, false );
+			
 		wp_enqueue_script( 'geop_q', plugin_dir_url( __FILE__ ) . 'js/q_2.0.3.js', array(), $this->version, false );
-		wp_enqueue_script( 'geop_bootstrap-js', plugin_dir_url( __FILE__ ) . 'js/bootstrap.bundle.min.js', array( 'jquery' ), $this->version, false );
+		// wp_enqueue_script( 'geop_bootstrap-js', plugin_dir_url( __FILE__ ) . 'js/bootstrap.bundle.min.js', array( 'jquery' ), $this->version, false );
 		wp_enqueue_script( 'geop_client_api', plugin_dir_url( __FILE__ ) . 'js/geoplatform.client.js', array(), $this->version, false );
 		wp_enqueue_script( 'geop_framework', plugin_dir_url( __FILE__ ) . 'js/geoplatform.js', array(), $this->version, false );
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/geoplatform-service-collector-public.js', array( 'jquery' ), $this->version, false );
