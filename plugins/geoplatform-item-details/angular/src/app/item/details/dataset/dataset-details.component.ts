7fe2f8d75e1bb14b7108e42d9f36093b13990c65
Patrick Neal <patrickn@imagemattersllc.com>
2019-06-18 09:37:39 -0400

    IDp - show basic eo properties for datasets with eo resource type; allow clone only if gp-editor

--- plugins/geoplatform-item-details/angular/src/app/item/details/dataset/dataset-details.component.ts
+++ plugins/geoplatform-item-details/angular/src/app/item/details/dataset/dataset-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+const EO_RT = 'http://www.geoplatform.gov/ont/dataset-types/EOImagery';
+
 @Component({
   selector: 'gpid-dataset-details',
   templateUrl: './dataset-details.component.html',
@@ -8,10 +10,16 @@ import { Component, OnInit, Input } from '@angular/core';
 export class DatasetDetailsComponent implements OnInit {
 
     @Input() item : any;
-    
+
     constructor() { }
 
     ngOnInit() {
+    }
+
+    isEO() : boolean {
+        return this.item && this.item.resourceTypes &&
+            this.item.resourceTypes.length &&
+            this.item.resourceTypes.indexOf(EO_RT) >= 0;
     }
 
 }
