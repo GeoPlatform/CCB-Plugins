5be0f283267e46b5bcdfbe90ef6872516673c0a9
Lee  Heazel <leeh@imagemattersllc.com>
2019-06-18 13:18:04 -0400

    DT-2655: feature deemed superfulous, removed.

--- themes/geoplatform-ccb/sub-header-post.php
+++ themes/geoplatform-ccb/sub-header-post.php
@@ -17,19 +17,7 @@ $geopccb_theme_options = geop_ccb_get_theme_mods();
 // If the exceprt is larger than 300 characters, breaks it in half while making
 // sure that the first half is as close to 300 as possible without going over.
 $geop_portal_excerpt_overflow = false;
-$geop_portal_excerpt_one = wp_strip_all_tags(wp_kses_post(get_post_meta($post->ID, 'geop_ccb_custom_wysiwyg', true)));
-$geop_portal_excerpt_two = wp_strip_all_tags(wp_kses_post(get_post_meta($post->ID, 'geop_ccb_custom_wysiwyg', true)));
-
-if (strlen($geop_portal_excerpt_one) > 300){
-  $geop_portal_excerpt_overflow = true;
-  $geop_portal_exploded_array = explode('.', $geop_portal_excerpt_one);
-  $geop_portal_excerpt_one = array_shift($geop_portal_exploded_array) . '.';
-  while (count($geop_portal_exploded_array) > 0 && (strlen($geop_portal_excerpt_one) + strlen($geop_portal_exploded_array[0]) < 300))
-    $geop_portal_excerpt_one = $geop_portal_excerpt_one . array_shift($geop_portal_exploded_array) . '.';
-}
-else {
-  $geop_portal_excerpt_one = $geop_portal_excerpt_two;
-}
+$geop_portal_excerpt = wp_strip_all_tags(wp_kses_post(get_post_meta($post->ID, 'geop_ccb_custom_wysiwyg', true)));
 
 // Outputs the community-links menu here if enabled here.
 if (has_nav_menu('community-links') && get_theme_mod('linkmenu_controls', $geopccb_theme_options['linkmenu_controls']) == 'below')
@@ -70,32 +58,6 @@ if (get_theme_mod('breadcrumb_controls', $geopccb_theme_options['breadcrumb_cont
 // is something to display. Uses HTML because of toggleClass.
 if ((wp_kses_post(get_post_meta($post->ID, 'geop_ccb_custom_wysiwyg', true)) != '' ) && (get_theme_mod('postbanner_controls', $geopccb_theme_options['postbanner_controls']) == 'off')){
   echo "<div class='m-page-overview collapsible' id='geopccb_subexcerpt_main'>";
-    echo $geop_portal_excerpt_one;
-    if ($geop_portal_excerpt_overflow){
-      echo "<button class='m-page-overview__toggle btn-account' id='geopccb_subexcerpt_main_toggle' style='background:transparent;border-color:transparent'>";
-        echo "<span class='fas fa-caret-down'></span>";
-      echo "</button>";
-    echo "</div>";
-    // echo "<div class='m-page-overview__additional'>";
-    echo "<div class='m-page-overview collapsible is-collapsed' id='geopccb_subexcerpt_supp'>";
-      echo $geop_portal_excerpt_two;
-      echo "<button class='m-page-overview__toggle' id='geopccb_subexcerpt_supp_toggle' style='background:transparent;border-color:transparent'>";
-        echo "<span class='fas fa-caret-down'></span>";
-      echo "</button>";
-    }
+    echo $geop_portal_excerpt;
   echo "</div>";
 }
-?>
-<script type="text/javascript">
-  jQuery(document).ready(function() {
-    jQuery("#geopccb_subexcerpt_main_toggle").click(function(event){
-      jQuery("#geopccb_subexcerpt_main").addClass('is-collapsed');
-      jQuery("#geopccb_subexcerpt_supp").removeClass('is-collapsed');
-    });
-
-    jQuery("#geopccb_subexcerpt_supp_toggle").click(function(event){
-      jQuery("#geopccb_subexcerpt_supp").addClass('is-collapsed');
-      jQuery("#geopccb_subexcerpt_main").removeClass('is-collapsed');
-    });
-	});
-</script>
