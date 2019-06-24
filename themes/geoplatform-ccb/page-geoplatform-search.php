92cb5c94ed68078b58a21b3959c8f680f3239241
Lee  Heazel <leeh@imagemattersllc.com>
2019-06-24 13:26:09 -0400

    DT-2674: OpenID Connect included. Checking functionality.

--- themes/geoplatform-ccb/page-geoplatform-search.php
+++ themes/geoplatform-ccb/page-geoplatform-search.php
@@ -11,6 +11,20 @@
 get_header();
 get_template_part( 'sub-header-post', get_post_format() );
 
+// $temp = get_user_meta(get_current_user_id(), 'openid-connect-generic-last-token-response', true);
+// var_dump($temp);
+// echo "<br>";
+// echo current_time( 'timestamp', TRUE );
+
+$manager = WP_Session_Tokens::get_instance( wp_get_current_user()->ID );
+$token = wp_get_session_token();
+$session = $manager->get( $token );
+var_dump($manager);
+echo "<br>";
+var_dump($token);
+echo "<br>";
+var_dump($session);
+
 if ( have_posts() ) : while ( have_posts() ) : the_post();
   the_content();
 endwhile; endif;
