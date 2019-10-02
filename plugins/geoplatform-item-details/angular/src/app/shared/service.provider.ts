99c4b2877819fd5e05be9340cca1a5319ad65118
Patrick Neal <patrickn@imagemattersllc.com>
2019-10-02 10:27:15 -0400

    use rpmservicefactory to create rpmservice; fix IDp not sending view events

--- plugins/geoplatform-item-details/angular/src/app/shared/service.provider.ts
+++ plugins/geoplatform-item-details/angular/src/app/shared/service.provider.ts
@@ -4,7 +4,8 @@ import {
     Config, ItemService, ServiceService, UtilsService, KGService
 } from '@geoplatform/client';
 import { NG2HttpClient } from '@geoplatform/client/angular';
-import { RPMService } from '@geoplatform/rpm/src/iRPMService'
+import { RPMService } from '@geoplatform/rpm/src/iRPMService';
+import { RPMServiceFactory } from '@geoplatform/rpm/dist/js/geoplatform.rpm.browser.js';
 import { environment } from '../../environments/environment';
 
 
@@ -13,7 +14,9 @@ var _rpmService : RPMService = null;
 
 export function rpmServiceFactory() {
     if(_rpmService) return _rpmService;
-    _rpmService = new RPMService(environment.rpmUrl, environment.rpmToken);
+    //rpm lib expects protocol-less urls
+    let href = environment.rpmUrl.replace('https://','');
+    _rpmService = RPMServiceFactory(href, environment.rpmToken);
     return _rpmService;
 }
 
--- plugins/geoplatform-map-preview/angular/src/app/shared/service.provider.ts
+++ plugins/geoplatform-map-preview/angular/src/app/shared/service.provider.ts
@@ -1,12 +1,15 @@
 import { HttpClient } from '@angular/common/http';
-import { RPMService } from '@geoplatform/rpm/src/iRPMService'
+import { RPMService } from '@geoplatform/rpm/src/iRPMService';
+import { RPMServiceFactory } from '@geoplatform/rpm/dist/js/geoplatform.rpm.browser.js';
 import { environment } from '../../environments/environment';
 
 var _rpmService : RPMService = null;
 
 export function rpmServiceFactory() {
     if(_rpmService) return _rpmService;
-    _rpmService = new RPMService(environment.rpmUrl, environment.rpmToken);
+    //rpm lib expects protocol-less urls
+    let href = environment.rpmUrl.replace('https://','');
+    _rpmService = RPMServiceFactory(href, environment.rpmToken);
     return _rpmService;
 }
 
